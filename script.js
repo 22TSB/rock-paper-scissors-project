fc60a858cd87a5d373f0c24a749c623100b6f4af
bogdan22tsb <bodan22tsb@gmail.com>
2024-08-13 13:55:20 +0300

    new feature, changed the random image to computer choice

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ const gameTitle = document.querySelector(".title");
 const resetBtn = document.querySelector(".title_btn");
 const human_score = document.querySelector("#human_score");
 const computer_score = document.querySelector("#computer_score");
+const computerLogo = document.querySelector(".box-random-computer");
+
+var DOM_computerChoice = document.createElement("img");
+var DOM_RANDOM = document.createElement("img");
+
+DOM_RANDOM.src = "src/questionmark.png";
+DOM_RANDOM.setAttribute("class", "random");
+
+
 var humanScore = 0;
 var computerScore = 0;
 const btn = document.createElement("button");
@@ -39,18 +48,32 @@ const playGame = (e) => {
         const humanChoice = e.target.getAttribute("class");
         const computerChoice = getComputerChoice();
         const score = playRound(humanChoice, computerChoice);
+        if (computerChoice === "scissors") {
+            DOM_computerChoice.src = `src/${computerChoice}.webp`;
+            DOM_computerChoice.setAttribute("class", computerChoice);
+        }
+        else {
+            DOM_computerChoice.src = `src/${computerChoice}.jpg`;
+            DOM_computerChoice.setAttribute("class", computerChoice); 
+        }
         if (score === 1) {
             humanScore++;
             gameTitle.lastElementChild.textContent = `You won da round!`;
             human_score.textContent = `human: ${humanScore}`;
+            computerLogo.firstElementChild.remove();
+            computerLogo.append(DOM_computerChoice);
         }
         else if (score === 2) {
             computerScore++;
             gameTitle.lastElementChild.textContent = `You lost da round!`
             computer_score.textContent = `computer: ${computerScore}`;
+            computerLogo.firstElementChild.remove();
+            computerLogo.append(DOM_computerChoice);
         }
         else {
             gameTitle.lastElementChild.textContent = `It's a tie!`
+            computerLogo.firstElementChild.remove();
+            computerLogo.append(DOM_computerChoice);
         }
         computerName.textContent = computerChoice;
         if (humanScore === 3) {
@@ -74,6 +97,8 @@ const resetFunction = () => {
     computerScore = 0;
     humanScore = 0;
     resetBtn.removeChild(btn);
+    computerLogo.firstElementChild.remove();
+    computerLogo.append(DOM_RANDOM);
     gameTitle.lastElementChild.textContent = "click on the item to start";
     human_score.textContent = "human";
     computer_score.textContent = "computer";
